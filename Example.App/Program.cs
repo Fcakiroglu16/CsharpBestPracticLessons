d0153f2772c8d93da2c2c679f12a6d1c76345490
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2024-08-11 22:24:55 +0300

    completed

---
+++ Example.App/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+    
+    
+// Null - conditional operator (?.)
+// If 'message' is null, 'length' will be null; otherwise, it will be the length of 'message'.
+string? message = null;
+int? length = message?.Length;
+Console.WriteLine(length); // Output: 
+
+// Null-coalescing operator (??)
+// If 'name' is null, 'displayName' will be "Default Name"; otherwise, it will be the value of 'name'.
+string? name = null;
+string displayName = name ?? "Default Name";
+Console.WriteLine(displayName); // Output: Default Name
+
+// Null-coalescing assignment operator (??=)
+// If 'greeting' is null, it will be assigned "Hello, World!"; otherwise, it retains its original value.
+string? greeting = null;
+greeting ??= "Hello, World!";
+Console.WriteLine(greeting); // Output: Hello, World!
---
+++ WebApplication.API/RecordAsEnumExample.cs
@@ -0,0 +1,38 @@
+﻿namespace WebApplication.API;
+
+public record ProductSize(int Value, string Name)
+{
+    public static ProductSize Small { get; } = new ProductSize(1, "Small");
+    public static ProductSize Medium { get; } = new ProductSize(2, "Medium");
+    public static ProductSize Large { get; } = new ProductSize(3, "Large");
+    public static ProductSize XLarge { get; } = new ProductSize(4, "Extra Large");
+    public static ProductSize XXLarge { get; } = new ProductSize(5, "Extra Extra Large");
+
+    public static implicit operator int(ProductSize size) => size.Value;
+
+    public static implicit operator ProductSize(int value) => value switch
+    {
+        1 => Small,
+        2 => Medium,
+        3 => Large,
+        4 => XLarge,
+        5 => XXLarge,
+        _ => throw new ArgumentOutOfRangeException(nameof(value), value, "Invalid value")
+    };
+    public bool IsSizeInLargeType() => this == Large || this == XLarge || this == XXLarge;
+    public override string ToString() => Name;
+}
+
+public class RecordAsEnumExample
+{
+    public void Run()
+    {
+        ProductSize productSize = ProductSize.Large;
+
+        int productSizeAsNumber = ProductSize.XXLarge;
+
+        ProductSize sizeAsProductSizeType = 1;
+
+        var ProductSizeAsName = ProductSize.XXLarge.ToString();
+    }
+}
